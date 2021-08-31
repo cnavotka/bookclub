88ba31028dc704b4d54845e84c3990adab2df954
Carlos Navotka <knavotka@hotmail.com>
2021-08-31 19:43:29 +0000

    Connect Flask to MongoDB and also created the home html file

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
+
+# Home
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/home")
+def home():
+    if "user" in session:
+        user = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+        return render_template(
+            "home.html", user=user)
+    else:
+        return render_template("home.html")
 
 
 if __name__ == "__main__":
