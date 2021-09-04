085be56c2495c9b19668c9788d8c4ecd55a28b02
Carlos Navotka <knavotka@hotmail.com>
2021-09-04 16:55:52 +0000

    Add delete book route to the app.py file

--- app.py
+++ app.py
@@ -151,6 +151,13 @@ def edit_book(book_id):
     return render_template("view_book.html", book=book)
 
 
+# Delete Book
+@app.route("/delete_book/<book_id>")
+def delete_book(book_id):
+    mongo.db.books.remove({"_id": ObjectId(book_id)})
+    return redirect("/profile/<username>")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
