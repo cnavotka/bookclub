3cd5588aa67b5af028cd35f99a27fbeef4c8159d
Carlos Navotka <knavotka@hotmail.com>
2021-12-17 13:29:32 +0000

    Change debug in app.py to True

--- app.py
+++ app.py
@@ -313,4 +313,4 @@ def search():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
